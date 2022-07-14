9b1a4107cb28e5ea61dccc7ff0f7f63498c33000
pallavgupta01 <pallav.gupta@technia.com>
2022-07-14 17:47:04 +0530

    Client : Correcting header for forms

--- client/src/pages/form/components/header/noneditable/HeaderSection.jsx
+++ client/src/pages/form/components/header/noneditable/HeaderSection.jsx
@@ -1,9 +1,17 @@
 import { Box, Typography } from '@mui/material';
 import React, { useContext } from 'react';
-import { QuestionsContext } from '../../../../../hooks/contexts';
+import {
+  QuestionsContext,
+  UserFormContext,
+} from '../../../../../hooks/contexts';
 
 const HeaderSection = () => {
-  const { header } = useContext(QuestionsContext);
+  const formData = useContext(UserFormContext);
+  const createContext = useContext(QuestionsContext);
+  let title = createContext?.header?.title;
+  if (!title) title = formData?.name;
+  let description = createContext?.header?.description;
+  if (!description) description = formData?.description;
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -14,9 +22,9 @@ const HeaderSection = () => {
             mb: 1,
           }}
         >
-          {header?.title}
+          {title}
         </Typography>
-        <Typography variant="subtitle1">{header?.description}</Typography>
+        <Typography variant="subtitle1">{description}</Typography>
       </div>
     </Box>
   );
